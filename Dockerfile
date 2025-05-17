a77b6a22a317fb3a1fb170b5eeb9b5f4c94fc4d8
suyash <suyashadhikari04@gmail.com>
2025-05-17 23:59:04 +0545

    Port changed to 80

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11-slim-buster
 WORKDIR /app
 COPY . /app
 
-
+EXPOSE 80
 RUN apt update -y 
 RUN apt-get update && pip install -r requirements.txt
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def trainRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
